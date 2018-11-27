08c19b97e6ce2f2cc260f6558be68f0db1400ca7
Yinka Ashafa <perpetualcoder@gmail.com>
2018-11-27 16:24:32 +0100

    Finish Hello App

---
+++ sandbox/hello_app.py
@@ -0,0 +1,10 @@
+# Hello App
+
+print("----------------------------------------")
+print("               Hello App                ")
+print("----------------------------------------")
+
+user_name = input("What is your name >> ")
+greeting = f"Nice to meet you {user_name}"
+
+print(greeting)
